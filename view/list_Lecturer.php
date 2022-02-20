9f33da47bb91ebe48edca3f06b53d9d43aff9c46
RonKu01 <87351766+RonKu01@users.noreply.github.com>
2022-02-20 14:23:20 +0800

    20220220

--- view/list_Lecturer.php
+++ view/list_Lecturer.php
@@ -29,7 +29,7 @@ if(!isset($_SESSION['unique_id'])){
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <label class="form-control form-control-dark w-100" style="text-align: center"><?php echo $_SESSION['roles']; ?>/label>
+    <label class="form-control form-control-dark w-100" style="text-align: center"><?php echo $_SESSION['roles']; ?></label>
     <div class="navbar-nav">
         <div class="nav-item text-nowrap">
             <a class="nav-link px-3" href="../controller/logout.php?logout_id=<?php echo $_SESSION['unique_id']; ?> ">Sign out</a>
